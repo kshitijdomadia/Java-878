693f6c33bd697e1163c99ff06cfef453d3abe5ab
Kshitij Domadia <kshitij.domadia@gmail.com>
2020-12-18 10:32:42 +0400

    hello im the new userr

--- GitProj/src/HelloWorld.java
+++ GitProj/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("hello");
 		System.out.println("second line");
 		System.out.println("3rd line");
+		System.out.println("Hi this is the second user");
 	}
 
 }
