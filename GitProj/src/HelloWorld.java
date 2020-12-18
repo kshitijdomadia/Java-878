438c716e2db68be6a14bb64028701bc9369387e1
Kshitij Domadia <kshitij.domadia@gmail.com>
2020-12-18 10:37:48 +0400

    namaste doston!

--- GitProj/src/HelloWorld.java
+++ GitProj/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("namaste");
 		System.out.println("second line");
 		System.out.println("3rd line");
 		System.out.println("Hi this is the second user");
